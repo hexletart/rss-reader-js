baa3946ee35a4f3506a33504da375afb1065c0d3
hexlet_art <hexlet_art@mail.ru>
2023-01-16 01:09:12 +0300

    added

---
+++ src/locales/en.js
@@ -0,0 +1,29 @@
+export default {
+  translation: {
+    aggregator: {
+      headerName: 'RSS aggregator',
+      description: "Start reading RSS today! It's easy and nice.",
+      form: {
+        inputLabel: 'RSS link',
+        submitLabel: 'Add',
+      },
+      fieldHint: 'Example: https://ru.hexlet.io/lessons.rss',
+      notifications: {
+        successes: {
+          rssUpload: 'RSS uploaded successfully',
+        },
+        errors: {
+          validation: {
+            existingRss: 'RSS already exists',
+            url: 'Link must be a valid URL',
+          },
+          process: {
+            notValidRss: 'The resource does not contain valid RSS',
+            axiosError: 'Network error',
+            internalError: 'Internal error',
+          },
+        },
+      },
+    },
+  },
+};
---
+++ src/locales/ru.js
@@ -0,0 +1,29 @@
+export default {
+  translation: {
+    aggregator: {
+      headerName: 'RSS агрегатор',
+      description: 'Начните читать RSS сегодня! Это легко, это красиво.',
+      form: {
+        inputLabel: 'Ссылка RSS',
+        submitLabel: 'Добавить',
+      },
+      fieldHint: 'Example: https://ru.hexlet.io/lessons.rss',
+      notifications: {
+        successes: {
+          rssUpload: 'RSS успешно загружен',
+        },
+        errors: {
+          validation: {
+            existingRss: 'RSS уже существует',
+            url: 'Ссылка должна быть валидным URL',
+          },
+          process: {
+            notValidRss: 'Ресурс не содержит валидный RSS',
+            axiosError: 'Ошибка сети',
+            internalError: 'Внутренняя ошибка',
+          },
+        },
+      },
+    },
+  },
+};
