08d7c1257dd58f4e89b3ede4610affd77255f1e1
hexlet_art <hexlet_art@mail.ru>
2023-01-25 14:48:25 +0300

    introduced animation for getting data and fixed code in handleNotification

--- src/scripts/view.js
+++ src/scripts/view.js
@@ -2,23 +2,28 @@
 
 // import _ from 'lodash';
 
-import handleResponseData from './handlers/handleData/handleResponseData';
+import handleResponseData from './handlers/handleResponseData';
+import handleSendingAnimation from './handlers/handleSendingAnimation';
 
 const handleProcess = (elements, process) => {
   switch (process) {
     case 'filling':
+      handleSendingAnimation(elements, false);
       elements.formSubmit.disabled = false;
       elements.input.disabled = false;
       break;
     case 'error':
+      handleSendingAnimation(elements, false);
       elements.formSubmit.disabled = true;
       elements.input.disabled = false;
       break;
     case 'sending':
+      handleSendingAnimation(elements, true);
       elements.formSubmit.disabled = true;
       elements.input.disabled = true;
       break;
     case 'sent':
+      handleSendingAnimation(elements, false);
       elements.formSubmit.disabled = false;
       elements.input.disabled = false;
       elements.form.reset();
@@ -35,17 +40,18 @@ const handleNotification = (elements, notificationData) => {
   const { notification } = elements;
   notification.textContent = '';
   notification.classList.remove('text-danger', 'text-success');
-  const getNoticeColor = (el) => ((el instanceof Error)
-    ? 'text-danger' : 'text-success');
   elements.input.classList.remove('is-invalid');
   if (!notice.message) return;
-  elements.input.classList.add('is-invalid');
-  notification.classList.add(getNoticeColor(notice));
+  if (notice instanceof Error) {
+    elements.input.classList.add('is-invalid');
+    notification.classList.add('text-danger');
+  }
+  notification.classList.add('text-success');
   notification.textContent = notice.message;
 };
 
 const getWordMapByElements = (words) => ({
-  formSubmit: words.t('aggregator.form.submit'),
+  formSubmitDescription: words.t('aggregator.form.submit'),
   header: words.t('aggregator.header'),
   description: words.t('aggregator.description'),
   hint: words.t('aggregator.hint'),
