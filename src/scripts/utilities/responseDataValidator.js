35b9586465a52600c3812b69e5d7875fb61d25ae
hexlet_art <hexlet_art@mail.ru>
2023-02-09 21:30:40 +0300

    trying to fix issues

--- src/scripts/utilities/responseDataValidator.js
+++ src/scripts/utilities/responseDataValidator.js
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 
 export default (responseData) => {
-  console.log('RESPONSE DATA ====> ', responseData);
+  console.log('\n', 'RESPONSE DATA ====> ', responseData.status, '\n');
   const { content_type: contentType, http_code: httpCode } = responseData.status;
   const rssRegex = /application\/rss\+xml/;
   const formatValidator = new RegExp(rssRegex);
