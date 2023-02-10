683c993a14c38e1edec7932397c836758c87a711
hexlet_art <hexlet_art@mail.ru>
2023-02-10 22:53:24 +0300

    trying to fix issues

--- src/scripts/utilities/axiosGetter.js
+++ src/scripts/utilities/axiosGetter.js
@@ -1,11 +1,14 @@
+/* eslint-disable no-console */
 import axios from 'axios';
 
 export default (url) => axios
   .get(`https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(url)}`)
   .then((response) => {
-    console.log('RESPONSE ===> ', response);
-    console.log('RESPONSE.HEADERS.GET("VIA") ==> ', response.headers.get('Via'));
-    console.log('RESPONSE.DATA ====> ', response.data);
+    // console.log('RESPONSE ===> ', response);
+    console.log('RESPONSE.HEADERS.GET("Via") ==> ', response.headers.get('Via'));
+    console.log('RESPONSE.HEADERS["via"] ==> ', response.headers['via']);
+    console.log('RESPONSE.HEADERS.VIA ==> ', response.headers.via);
+    // console.log('RESPONSE.DATA ====> ', response.data);
     return response.data;
   })
   .catch((err) => { throw err; });
