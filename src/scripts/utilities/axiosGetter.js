09b4725a13ce9de3ac5c8531620797d4020340e9
hexlet_art <hexlet_art@mail.ru>
2023-02-10 14:25:27 +0300

    trying to fix issues

--- src/scripts/utilities/axiosGetter.js
+++ src/scripts/utilities/axiosGetter.js
@@ -2,5 +2,9 @@ import axios from 'axios';
 
 export default (url) => axios
   .get(`https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(url)}`)
-  .then((response) => response.data)
+  .then((response) => {
+    console.log('RESPONSE ===> ', response);
+    console.log('RESPONSE.DATA ====> ', response.data);
+    return response.data;
+  })
   .catch((err) => { throw err; });
--- src/scripts/utilities/responseDataValidator.js
+++ src/scripts/utilities/responseDataValidator.js
@@ -1,9 +1,9 @@
 import _ from 'lodash';
 
 export default (responseData) => {
-  console.log('\n', 'RESPONSE ====> ', responseData, '\n');
-  console.log('\n', 'RESPONSE STATUS ====> ', responseData.status, '\n');
-  console.log('\n', 'RESPONSE CONTENTS ====> ', responseData.contents, '\n');
+  // console.log('\n', 'RESPONSE ====> ', responseData, '\n');
+  // console.log('\n', 'RESPONSE STATUS ====> ', responseData.status, '\n');
+  // console.log('\n', 'RESPONSE CONTENTS ====> ', responseData.contents, '\n');
 
   const { content_type: contentType, http_code: httpCode } = responseData.status;
   const rssRegex = /application\/rss\+xml/;
