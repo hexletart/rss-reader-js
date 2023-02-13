9a19baec1a87fa7f1ed63a5fcf75dff77eac3da0
hexlet_art <hexlet_art@mail.ru>
2023-02-13 21:35:45 +0300

    added two additional elements to fill with words

--- src/scripts/handlers/handleBaseView.js
+++ src/scripts/handlers/handleBaseView.js
@@ -7,10 +7,15 @@ const getWordMapByElements = (words) => ({
   formLabel: words.t('aggregator.form.label'),
   modalButtonRead: words.t('aggregator.modalComponent.btnRead'),
   modalButtonClose: words.t('aggregator.modalComponent.btnClose'),
+  formSwitchLabel: words.t('aggregator.languageButton.passive'),
+  footerText: words.t('aggregator.footerText'),
 });
 
 export default (elements, wordHandler) => {
   const wordMap = getWordMapByElements(wordHandler);
   Object.entries(wordMap)
-    .forEach(([key, value]) => { elements[key].textContent = value; });
+    .forEach(([key, value]) => {
+      elements[key].textContent = '';
+      elements[key].textContent = value;
+    });
 };
