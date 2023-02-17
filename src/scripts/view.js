be04fd5d64a18b98a79f4bb559c8852dbdcfb98c
hexlet_art <hexlet_art@mail.ru>
2023-02-17 21:43:41 +0300

    added new case to change language and added function for modal window

--- src/scripts/view.js
+++ src/scripts/view.js
@@ -3,23 +3,38 @@ import handleBaseView from './handlers/handleBaseView';
 import handleVisitedPost from './handlers/handleVisitedPost';
 import handleNotification from './handlers/handleNotification';
 import handleButtonLng from './handlers/handleButtonLng';
+import handleDataForModal from './handlers/handleDataForModal';
 import { handleFeeds, handlePosts } from './handlers/handleResponseData';
 
 export default (watchedState, elements, wordHandler, path, value) => {
+  const { formNotifications } = watchedState.form;
   const regexByVisitedChange = /^form\.response\.posts\.(\d+)$/;
-  if (regexByVisitedChange.test(path)) return handleVisitedPost(elements, value);
+  if (regexByVisitedChange.test(path)) {
+    handleDataForModal(elements, value, wordHandler);
+    handleVisitedPost(elements, value);
+  }
   switch (path) {
-    case 'lng': handleBaseView(elements, wordHandler);
+    case 'lng':
+      handleBaseView(elements, wordHandler);
+      handleNotification(
+        elements,
+        (typeof formNotifications === 'function' ? formNotifications.bind(watchedState) : formNotifications),
+      );
       break;
-    case 'form.processState': handleProcess(elements, value);
+    case 'form.processState':
+      handleProcess(elements, value);
       break;
-    case 'form.response.feeds': handleFeeds(elements, wordHandler, value);
+    case 'form.response.feeds':
+      handleFeeds(elements, wordHandler, value);
       break;
-    case 'form.response.posts': handlePosts(watchedState, elements, wordHandler, value);
+    case 'form.response.posts':
+      handlePosts(watchedState, elements, wordHandler, value);
       break;
-    case 'form.formNotifications': handleNotification(elements, value);
+    case 'form.formNotifications':
+      handleNotification(elements, value);
       break;
-    case 'form.uiButtonLng': handleButtonLng(elements, value, wordHandler);
+    case 'form.uiButtonLng':
+      handleButtonLng(elements, value, wordHandler);
       break;
     default:
       break;
