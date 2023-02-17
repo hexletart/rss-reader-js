5eb1f6a16dfce181b63d8944a01d38859935ac1e
hexlet_art <hexlet_art@mail.ru>
2023-02-17 21:40:29 +0300

    corrected by new error algorithm

--- src/scripts/validation/schema.js
+++ src/scripts/validation/schema.js
@@ -1,14 +1,9 @@
 import * as yup from 'yup';
 
-export default (disallowedValues, wordHandler) => {
+export default (disallowedValues) => {
   yup.setLocale({
-    string: {
-      url: wordHandler.t('aggregator.notifications.errors.validation.url'),
-    },
-    mixed: {
-      required: wordHandler.t('aggregator.notifications.errors.validation.empty'),
-      notOneOf: wordHandler.t('aggregator.notifications.errors.validation.notOneOf'),
-    },
+    string: { url: 'url' },
+    mixed: { required: 'required', notOneOf: 'notOneOf' },
   });
   return yup.object().shape({
     link: yup.string().required().url().notOneOf(disallowedValues),
--- src/scripts/validation/validate.js
+++ src/scripts/validation/validate.js
@@ -1,7 +1,7 @@
 import getSchema from './schema';
 
-export default (inputValue, disallowedValues, wordHandler) => {
-  const schema = getSchema(disallowedValues, wordHandler);
+export default (inputValue, disallowedValues) => {
+  const schema = getSchema(disallowedValues);
   return schema.validate(inputValue, { abortEarly: false })
     .then(() => ({}))
     .catch((err) => err);
