243ee948701c45f6a8a34c0ee34396a9150dabfa
hexlet_art <hexlet_art@mail.ru>
2023-02-17 21:36:19 +0300

    seperated algorithm for current module

--- src/scripts/handlers/handleVisitedPost.js
+++ src/scripts/handlers/handleVisitedPost.js
@@ -1,11 +1,6 @@
 /* eslint-disable object-curly-newline */
 export default (elements, value) => {
-  const { postTitle, postDescription, postLink, id } = value;
-  const { modalTitle, modalBody, modalButtonRead } = elements;
-  const visitedPost = elements.posts.querySelector(`a[data-id="${id}"]`);
+  const visitedPost = elements.posts.querySelector(`a[data-id="${value.id}"]`);
   visitedPost.classList.remove('fw-bold');
   visitedPost.classList.add('fw-normal', 'opacity-75');
-  modalTitle.textContent = postTitle;
-  modalBody.innerHTML = postDescription;
-  modalButtonRead.href = postLink;
 };
