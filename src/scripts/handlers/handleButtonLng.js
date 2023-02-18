4d6db47b7989b3b81d6696062ff316b32f0e57eb
hexlet_art <hexlet_art@mail.ru>
2023-02-18 15:44:15 +0300

    revised hanging of certain classes

--- src/scripts/handlers/handleButtonLng.js
+++ src/scripts/handlers/handleButtonLng.js
@@ -1,15 +1,14 @@
 export default (elements, butStatus, wordHandler) => {
   const { formSwitchLabel: button } = elements;
   button.textContent = '';
-  button.classList.remove('transition-close', 'transition-open');
+  button.classList.remove('transformation-open');
   switch (butStatus) {
     case 'active':
       button.textContent = wordHandler.t('aggregator.languageButton.active');
-      button.classList.add('transition-open');
+      button.classList.add('transformation-open');
       break;
     case 'passive':
       button.textContent = wordHandler.t('aggregator.languageButton.passive');
-      button.classList.add('transition-close');
       break;
     default: throw Error(`Unknown status - ${butStatus} in handleButtonLng`);
   }
